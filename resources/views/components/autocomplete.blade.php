52dc7f8addabc0669735f23ffd7180600ac7d22d
Josh Hanley <josh@hitsystems.com.au>
2021-01-04 16:56:42 +1100

    Add test for loading indicator

--- resources/views/components/autocomplete.blade.php
+++ resources/views/components/autocomplete.blade.php
@@ -56,7 +56,15 @@ $attributes = $attributes->except(['wire:input-property', 'wire:results-property
         </div>
     </div>
 
-    <div x-show="showDropdown && hasResults()" x-on:click="selectItem()" x-on:mouseleave="focusIndex = null" dusk="autocomplete-dropdown" x-cloak>
+    <div x-show="showDropdown && hasResults()" x-on:click="selectItem()" x-on:mouseleave="focusIndex = null" class="relative" dusk="autocomplete-dropdown" x-cloak>
+        <div wire:loading.delay.class.remove="hidden" class="hidden absolute inset-0 flex items-center justify-center" dusk="autocomplete-loading">
+            <div class="absolute inset-0 bg-gray-500 opacity-25"></div>
+            <svg class="animate-spin h-4 w-4 text-cool-gray-700 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
+
         @foreach ($this->getPropertyValue($resultsProperty) as $key => $result)
             <div
                 wire:key="result-{{ $key }}"
---
+++ tests/Browser/AutocompleteLoadingTest/AutocompleteWithLoadingComponent.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LivewireAutocomplete\Tests\Browser\AutocompleteLoadingTest;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+class AutocompleteWithLoadingComponent extends Component
+{
+    public $results = [
+        'bob',
+        'john',
+        'bill'
+    ];
+
+    public $input = '';
+
+    public $selected;
+
+    public function calculateResults()
+    {
+        $this->reset('results');
+
+        $this->results = Collection::wrap($this->results)
+            ->filter(function ($result) {
+                if (!$this->input) {
+                    return true;
+                }
+
+                return str_contains($result, $this->input);
+            })
+            ->values()
+            ->toArray();
+    }
+
+    public function updatedInput()
+    {
+        sleep(5);
+        $this->calculateResults();
+    }
+
+    public function updatedSelected()
+    {
+        $this->input = $this->selected ?? null;
+
+        $this->calculateResults();
+    }
+
+    public function render()
+    {
+        return <<<'HTML'
+            <div dusk="page">
+                <x-lwc::autocomplete
+                    wire:input-property="input"
+                    wire:selected-property="selected"
+                    wire:results-property="results"
+                    />
+
+                <div dusk="result-output">{{ $selected }}</div>
+            </div>
+            HTML;
+    }
+}
