e2626b85c389d361d6cb84d049b5148a96a19174
Josh Hanley <josh@hitsystems.com.au>
2021-06-11 16:31:47 +1000

    Update clear button styling

--- resources/views/components/autocomplete.blade.php
+++ resources/views/components/autocomplete.blade.php
@@ -60,9 +60,9 @@
 
         <div x-on:click="clearItem($dispatch)" class="absolute right-0 inset-y-0 flex items-center">
             <button x-show="selected" type="button" class="group focus:outline-none" dusk="clear" x-cloak>
-                <div class="mr-2">
-                    <svg class="h-5 w-5 border-2 rounded group-focus:border-blue-400 text-gray-700 fill-current transition-transform ease-in-out duration-100 transform hover:scale-105 hover:text-black"
-                         viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div class="mr-3">
+                    <svg class="h-5 w-5 border-2 border-gray-300 rounded group-focus:border-blue-400 bg-white text-gray-700 fill-current transition-transform ease-in-out duration-100 transform hover:scale-105 hover:text-black"
+                        viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"></path>
                     </svg>
                 </div>
