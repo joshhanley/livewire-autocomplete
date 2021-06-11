9d9111b2e4aca86c3106fd5a76d418f4b7c51014
Josh Hanley <josh@hitsystems.com.au>
2021-06-11 15:34:11 +1000

    Remove unused code

--- resources/views/components/autocomplete.blade.php
+++ resources/views/components/autocomplete.blade.php
@@ -17,8 +17,6 @@
     $attributes = $attributes->except(['wire:input-property', 'wire:results-property', 'wire:selected-property', 'wire:options-property'])
 @endphp
 
-{{-- @dd(get_defined_vars()) --}}
-
 <div
     x-data="autocomplete({
         name: {{ json_encode($name) }},
@@ -56,26 +54,17 @@
             dusk="autocomplete-input"
             x-bind:disabled="selected"
             x-spread="inputListeners()"
-            {{-- @if ($this->getPropertyValue($selectedProperty)) disabled @endif --}}
         />
 
         <div x-on:click="clearItem($dispatch)" class="absolute right-0 inset-y-0 flex items-center">
-            {{-- @if ($this->getPropertyValue($selectedProperty)) --}}
             <button x-show="selected" type="button" class="group focus:outline-none" dusk="clear" x-cloak>
-                {{-- @if ($clear)
-                    {{ $clear }}
-                    @else --}}
                 <div class="mr-2">
                     <svg class="h-5 w-5 border-2 rounded group-focus:border-blue-400 text-gray-700 fill-current transition-transform ease-in-out duration-100 transform hover:scale-105 hover:text-black"
                          viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"></path>
                     </svg>
                 </div>
-                {{--
-            @endif --}}
             </button>
-            {{--
-        @endif --}}
         </div>
     </div>
 
@@ -235,10 +224,6 @@
                 totalResults() {
                     if (this.resultsCount) return this.resultsCount //Use memoised count
 
-                    // if (this.isGrouped) {
-                    //     return this.resultsCount = this.totalGroupedResults()
-                    // }
-
                     return this.resultsCount = this.results.length
                 },
 
@@ -300,7 +285,6 @@
 
                 selectItem($dispatch) {
                     if (this.hasFocus() && this.hasResults()) {
-                        console.log(this.focusIndex)
                         this.setSelected($dispatch, this.results[this.focusIndex])
                     } else {
                         if (this.autoselect) {
@@ -331,8 +315,6 @@
                 inputListeners() {
                     return {
                         ['x-on:' + (this.name ?? 'autocomplete') + '-clear.window'](event) {
-                            console.log(this)
-                            console.log(this.$el.__x.getDispatchFunction(event.target))
                             this.clearItem(this.$el.__x.getDispatchFunction(event.target))
                         },
                         ['x-on:' + (this.name ?? 'autocomplete') + '-set.window'](event) {
