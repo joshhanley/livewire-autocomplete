b13619c4ef56991107a1359f0464cbd54b375697
Josh Hanley <josh@hitsystems.com.au>
2021-07-03 10:38:52 +1000

    Format file

--- src/LivewireAutocompleteServiceProvider.php
+++ src/LivewireAutocompleteServiceProvider.php
@@ -14,37 +14,37 @@ class LivewireAutocompleteServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/' . $this->name . '.php', $this->name
+            __DIR__ . '/../config/' . $this->name . '.php',
+            $this->name
         );
     }
 
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/' . $this->name . '.php' => config_path($this->name . '.php')
+            __DIR__ . '/../config/' . $this->name . '.php' => config_path($this->name . '.php'),
         ], [$this->name, $this->name . '-config']);
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/' . $this->name)
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/' . $this->name),
         ], [$this->name, $this->name . '-views']);
 
         $this->loadViews(__DIR__ . '/../resources/views', $this->namespace, config($this->name . '.use_global_namespace', false));
 
-        if(config($this->name . '.use_global_namespace', false)) {
+        if (config($this->name . '.use_global_namespace', false)) {
             Blade::component(Autocomplete::class, null);
         } else {
             Blade::componentNamespace('LivewireAutocomplete\\Components', $this->namespace);
         }
-
     }
 
     protected function loadViews($path, $namespace, $useGlobalNamespace = false)
     {
         $this->callAfterResolving('view', function ($view) use ($path, $namespace, $useGlobalNamespace) {
-            if (isset($this->app->config['view']['paths']) &&
-                is_array($this->app->config['view']['paths'])) {
+            if (isset($this->app->config['view']['paths'])
+                && is_array($this->app->config['view']['paths'])) {
                 foreach ($this->app->config['view']['paths'] as $viewPath) {
-                    if (is_dir($appPath = $viewPath.'/vendor/' . $this->name)) {
+                    if (is_dir($appPath = $viewPath . '/vendor/' . $this->name)) {
                         $useGlobalNamespace
                             ? $view->addLocation($appPath)
                             : $view->addNamespace($namespace, $appPath);
