7e616d32c334afcd6e8f909051952a736eb3a581
Josh Hanley <josh@hitsystems.com.au>
2021-07-04 13:07:38 +1000

    Add custom macro for assessing visibility inside another element

--- tests/TestServiceProvider.php
+++ tests/TestServiceProvider.php
@@ -3,6 +3,8 @@
 namespace LivewireAutocomplete\Tests;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\Browser;
+use PHPUnit\Framework\Assert as PHPUnit;
 
 class TestServiceProvider extends ServiceProvider
 {
@@ -10,5 +12,49 @@ class TestServiceProvider extends ServiceProvider
     {
         config()->set('autocomplete.options.auto_select', false);
         config()->set('autocomplete.options.allow_new', false);
+
+        $this->addDuskMacros();
+    }
+
+    public function addDuskMacros()
+    {
+        $script = '
+            let elRect = document.querySelector(`%1$s`).getBoundingClientRect()
+            let containerRect = document.querySelector(`%2$s`).getBoundingClientRect()
+
+            return containerRect.top < elRect.bottom && containerRect.bottom > elRect.top
+        ';
+
+        Browser::macro('isVisibleInContainer', function ($selector, $container) use ($script) {
+            /** @var \Laravel\Dusk\Browser $this */
+            $fullSelector = $this->resolver->format($selector);
+            $fullContainer = $this->resolver->format($container);
+
+            $this->resolver->findOrFail($selector);
+            $this->resolver->findOrFail($container);
+
+            PHPUnit::assertTrue(
+                $this->driver->executeScript(sprintf($script, $fullSelector, $fullContainer)),
+                "Element [{$fullSelector}] is not visible in [{$fullContainer}]"
+            );
+
+            return $this;
+        });
+
+        Browser::macro('isNotVisibleInContainer', function ($selector, $container) use ($script) {
+            /** @var \Laravel\Dusk\Browser $this */
+            $fullSelector = $this->resolver->format($selector);
+            $fullContainer = $this->resolver->format($container);
+
+            $this->resolver->findOrFail($selector);
+            $this->resolver->findOrFail($container);
+
+            PHPUnit::assertFalse(
+                $this->driver->executeScript(sprintf($script, $fullSelector, $fullContainer)),
+                "Element [{$fullSelector}] is visible in [{$fullContainer}]"
+            );
+
+            return $this;
+        });
     }
 }
