a6ba084aebc7a7e2eafc4a5989b0b737a1f5d83d
Josh Hanley <josh@hitsystems.com.au>
2021-07-04 16:00:47 +1000

    Fix autocomplete attribute should actually be off

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,6 +1,6 @@
 <input
     type="text"
-    autocomplete="none"
+    autocomplete="off"
     {{ $attributes->class('w-full pl-4 py-2 rounded border border-cool-gray-200 shadow-inner leading-5 text-cool-gray-900 placeholder-cool-gray-400 focus:outline-none focus:border-blue-400') }}
     x-bind:class="[selected ? 'pr-9' : 'pr-4']"
     x-bind:disabled="selected" />
