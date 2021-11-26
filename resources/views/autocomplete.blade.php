ae4c9a34d993f2dfeb621da2d122ce47e3bad1f5
Tom White <info@aeroquote.com>
2021-11-26 17:28:43 +1000

    WIP

--- resources/views/autocomplete.blade.php
+++ resources/views/autocomplete.blade.php
@@ -128,7 +128,7 @@ $inline = filter_var($getOption('inline'), FILTER_VALIDATE_BOOLEAN);
                     this.$watch('focusIndex', () => this.scrollFocusedIntoView())
 
                     this.$watch('value', function () {
-                        console.log('this.$watch value');
+                        console.log('this.$watch value = '. value);
                     })
                 },
 
