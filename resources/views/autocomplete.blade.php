89dc55ae398632df9c544189de58500878b94dc7
Tom White <info@aeroquote.com>
2021-11-28 21:31:08 +1000

    set debouncedValue null when selected an item

--- resources/views/autocomplete.blade.php
+++ resources/views/autocomplete.blade.php
@@ -369,6 +369,7 @@ $inline = filter_var($getOption('inline'), FILTER_VALIDATE_BOOLEAN);
                 },
 
                 setSelected($dispatch, selected) {
+                    this.debouncedValue = null
                     this.value = typeof selected === 'object' && selected.hasOwnProperty(this.searchAttribute) ? selected[this.searchAttribute] : selected
                     this.selected = typeof selected === 'object' && selected.hasOwnProperty(this.idAttribute) ? selected[this.idAttribute] : selected
                     $dispatch((this.name ?? 'autocomplete') + '-selected-object', selected)
