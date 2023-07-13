07bc437be43e00ad8d7024e4eb9e22ba7cdcd5d5
Josh Hanley <josh@hitsystems.com.au>
2023-07-14 09:25:43 +1000

    Format app layout

--- tests/views/components/layouts/app.blade.php
+++ tests/views/components/layouts/app.blade.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <style>
-    [x-cloak] {
-        display: none;
-    }
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 
-    .bg-blue-500 {
-        background-color: #3b82f6;
-    }
+    <style>
+        [x-cloak] {
+            display: none;
+        }
+
+        .bg-blue-500 {
+            background-color: #3b82f6;
+        }
     </style>
 </head>
+
 <body>
     {{ $slot }}
 </body>
+
 </html>
