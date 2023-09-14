6eedbd6c02c87997dde671c1d243d167e9dcf2c9
Josh Hanley <josh@hitsystems.com.au>
2023-09-14 17:59:26 +1000

    Set up tests for running with V3

--- tests/Browser/TestCase.php
+++ tests/Browser/TestCase.php
@@ -2,21 +2,62 @@
 
 namespace LivewireAutocomplete\Tests\Browser;
 
+use Livewire\Features\SupportTesting\BaseTestCase;
 use LivewireAutocomplete\LivewireAutocompleteServiceProvider;
 use LivewireAutocomplete\Tests\TestServiceProvider;
 use LivewireDuskTestbench\TestCase as LivewireDuskTestbenchTestCase;
 
-class TestCase extends LivewireDuskTestbenchTestCase
+use function Livewire\trigger;
+
+class TestCase extends BaseTestCase
 {
-    public $withoutUI = false;
+    // public $withoutUI = false;
 
-    public $packageProviders = [
-        LivewireAutocompleteServiceProvider::class,
-        TestServiceProvider::class,
-    ];
+    // public $packageProviders = [
+    //     LivewireAutocompleteServiceProvider::class,
+    //     TestServiceProvider::class,
+    // ];
 
-    public function configureViewsDirectory()
+    // public function configureViewsDirectory()
+    // {
+    //     $this->viewsDirectory = __DIR__ . '/views';
+    // }
+
+    protected function getPackageProviders($app)
     {
-        $this->viewsDirectory = __DIR__ . '/views';
+        return [
+            ...parent::getPackageProviders($app),
+            LivewireAutocompleteServiceProvider::class,
+            TestServiceProvider::class
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+        
+        $app['config']->set('view.paths', [
+            __DIR__ . '/views',
+            resource_path('views'),
+        ]);
+    }
+
+    public static function tweakApplicationHook()
+    {
+        return function () {};
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        trigger('browser.testCase.setUp', $this);
+    }
+
+    public function tearDown(): void
+    {
+        trigger('browser.testCase.tearDown', $this);
+
+        parent::tearDown();
     }
 }
