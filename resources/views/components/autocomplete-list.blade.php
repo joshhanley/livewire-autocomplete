4e5527f56442b0892a0323bd902560a14426f1af
Josh Hanley <josh@hitsystems.com.au>
2023-10-26 14:19:47 +1000

    Add inline list dropdown option

--- resources/views/components/autocomplete-list.blade.php
+++ resources/views/components/autocomplete-list.blade.php
@@ -1,8 +1,9 @@
 @props([
     'unstyled' => false,
+    'inline' => false,
 ])
 
-<div class="absolute w-full">
+<div @class(['absolute' => !$inline, 'w-full'])>
     <ul
         x-show="open"
         {{ $attributes->class([!$unstyled => 'border border-gray-300 rounded bg-white relative overflow-hidden']) }}>
