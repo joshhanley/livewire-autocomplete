2681621abd034947b78e6ffb76c53c25484fb74a
Josh Hanley <josh@hitsystems.com.au>
2024-01-04 18:55:45 +1000

    Disable toggle on click

--- resources/views/components/autocomplete-input.blade.php
+++ resources/views/components/autocomplete-input.blade.php
@@ -29,7 +29,7 @@
     x-on:keydown.home.prevent="focusFirst()"
     x-on:keydown.end.prevent="focusLast()"
     x-on:keydown.enter.stop="enter($event)"
-    x-on:click="!wasJustFocused && toggle(); wasJustFocused = false"
+    {{-- x-on:click="!wasJustFocused && toggle(); wasJustFocused = false" --}}
     {{ $attributes->whereStartsWith('class')->class(['border border-gray-300 rounded w-full px-3 py-2' => !$unstyled]) }}
     >
     <input
