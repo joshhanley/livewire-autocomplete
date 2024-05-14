849d3a522592f7a7c269a3b0a5e003f4f29b2198
Josh Hanley <josh@hitsystems.com.au>
2024-05-14 21:51:37 +1000

    Remove dd

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -27,8 +27,6 @@ class ServiceProvider extends BaseServiceProvider
             __DIR__ . '/../config/' . $this->name . '.php' => config_path($this->name . '.php'),
         ], [$this->name, $this->name . '-config']);
 
-        // dd($this->namespace, config($this->name . '.use_global_namespace', true));
-
         $this->loadViews(__DIR__ . '/../resources/views', $this->namespace, config($this->name . '.use_global_namespace', true));
 
         if (config($this->name . '.use_global_namespace', true)) {
