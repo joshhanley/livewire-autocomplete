03fdd26926900f5695c5e5a5c4a722322ef70c1f
Josh Hanley <josh@hitsystems.com.au>
2024-05-16 17:59:37 +1000

    Update list styles

--- resources/views/components/autocomplete-list.blade.php
+++ resources/views/components/autocomplete-list.blade.php
@@ -1,12 +1,13 @@
 @props([
     'unstyled' => false,
     'inline' => false,
+    'containerClass' => '',
 ])
 
-<div @class(['absolute z-10' => !$inline, 'w-full'])>
+<div @class([$containerClass, 'absolute z-10' => !$inline && !$unstyled, 'w-full' => !$unstyled])>
     <ul
         x-show="open"
-        {{ $attributes->class(['mx-2 mt-1 border border-gray-300 rounded bg-white relative overflow-hidden' => !$unstyled]) }}
+        {{ $attributes->class(['mx-2 mt-1 border border-gray-300 rounded bg-white relative overflow-auto' => !$unstyled]) }}
         x-cloak>
         {{ $slot }}
     </ul>
