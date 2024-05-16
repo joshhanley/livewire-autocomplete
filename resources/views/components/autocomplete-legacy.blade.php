a88b111dcce1d24c448c438394a4619e8041712f
Josh Hanley <josh@hitsystems.com.au>
2024-05-16 21:35:56 +1000

    Keep implementing legacy compatibility component

--- resources/views/components/autocomplete-legacy.blade.php
+++ resources/views/components/autocomplete-legacy.blade.php
@@ -5,34 +5,47 @@
 ])
 
 @php
-$getOption = fn ($option) => $options[$option] ?? null;
-$hasResults = fn ($results) => is_countable($results) && count($results) > 0;
-$hasInputText = fn ($inputText) => $inputText !== null && $inputText != '';
-$shouldShowPlaceholder = fn ($results, $inputText) => ! $hasResults($results) && ! $hasInputText($inputText);
+    $defaultOptions = [
+        'id' => 'id',
+        'text' => 'text',
+        'auto-select' => true,
+        'allow-new' => true,
+        'load-once-on-focus' => true,
+        'inline' => false,
+        'inline-styles' => 'relative',
+        'overlay-styles' => 'absolute z-30',
+        'result-focus-styles' => 'bg-blue-500',
+    ];
+    $options = array_merge($defaultOptions, $options);
+    $getOption = fn($option) => $options[$option] ?? null;
+    $hasResults = fn($results) => is_countable($results) && count($results) > 0;
+    $hasInputText = fn($inputText) => $inputText !== null && $inputText != '';
+    $shouldShowPlaceholder = fn($results, $inputText) => !$hasResults($results) && !$hasInputText($inputText);
 
-$inputProperty = $attributes->wire('model-text');
-$resultsProperty = $attributes->wire('model-results');
-$selectedProperty = $attributes->wire('model-id');
-$focusAction = $attributes->wire('focus');
+    $inputProperty = $attributes->wire('model-text');
+    $resultsProperty = $attributes->wire('model-results');
+    $selectedProperty = $attributes->wire('model-id');
+    $focusAction = $attributes->wire('focus');
 
-/** Remove all wire attributes that are assigned to local properties from the attribute bag */
-$attributes = $attributes->whereDoesntStartWith('wire:');
+    /** Remove all wire attributes that are assigned to local properties from the attribute bag */
+    $attributes = $attributes->whereDoesntStartWith('wire:');
 
-$inputValue = $this->getPropertyValue($inputProperty->value);
-$resultsValue = $this->getPropertyValue($resultsProperty->value);
-$autoSelect = filter_var($getOption('auto-select'), FILTER_VALIDATE_BOOLEAN);
-$allowNew = filter_var($getOption('allow-new'), FILTER_VALIDATE_BOOLEAN);
-$loadOnceOnFocus = filter_var($getOption('load-once-on-focus'), FILTER_VALIDATE_BOOLEAN);
-$inline = filter_var($getOption('inline'), FILTER_VALIDATE_BOOLEAN);
+    $inputValue = $this->getPropertyValue($inputProperty->value);
+    $resultsValue = $this->getPropertyValue($resultsProperty->value);
+    $autoSelect = filter_var($getOption('auto-select'), FILTER_VALIDATE_BOOLEAN);
+    $allowNew = filter_var($getOption('allow-new'), FILTER_VALIDATE_BOOLEAN);
+    $loadOnceOnFocus = filter_var($getOption('load-once-on-focus'), FILTER_VALIDATE_BOOLEAN);
+    $inline = filter_var($getOption('inline'), FILTER_VALIDATE_BOOLEAN);
 @endphp
 
 {{-- @todo: Fix this so it supports namespacing --}}
-<x-autocomplete :wire:model.live="$selectedProperty->value">
-    <x-autocomplete-input :wire:model.live="$inputProperty->value" class="bg-white" />
+<x-autocomplete :auto-select="$autoSelect" :wire:model.live="$selectedProperty->value">
+    <x-autocomplete-input :wire:model.live="$inputProperty->value" class="bg-white" x-bind:disabled="id" />
 
     {{-- clear-button --}}
     <div x-show="id" class="absolute right-0 inset-y-0 pr-3 flex items-center" x-cloak>
         <button type="button"
+            x-on:click="clear"
             class="border-2 border-gray-300 rounded bg-white text-gray-700 transition-transform ease-in-out duration-100 transform hover:scale-105 hover:text-black focus:outline-none focus:border-blue-400">
             <svg class="h-5 w-5 fill-current"
                 viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -49,7 +62,7 @@ $inline = filter_var($getOption('inline'), FILTER_VALIDATE_BOOLEAN);
             </div>
         @else
             @if ($hasResults($resultsValue) || $allowNew)
-                @if ($allowNew) && strlen($inputValue) > 0)
+                @if ($allowNew  && strlen($inputValue) > 0)
                     <x-autocomplete-new-item :key="0" :value="$inputValue" wire:key='{{ $name }}-add-new'>
                         Add new "{{ $inputValue }}"
                     </x-autocomplete-new-item>
@@ -58,8 +71,8 @@ $inline = filter_var($getOption('inline'), FILTER_VALIDATE_BOOLEAN);
                 @if ($resultsValue)
                     @foreach ($resultsValue as $key => $result)
                         {{-- @todo: Change these to get from options if available --}}
-                        <x-autocomplete-item :key="$result->id" :value="$result->name" wire:key="{{ $name }}-result-{{ $key }}">
-                            {{ $result->name }}
+                        <x-autocomplete-item :key="$result[$getOption('id')] ?? $key" :value="$result[$getOption('text')] ?? $result" wire:key="{{ $name }}-result-{{ $key }}">
+                            {{  $result[$getOption('text')] ?? $result }}
                         </x-autocomplete-item>
                     @endforeach
                 @endif
