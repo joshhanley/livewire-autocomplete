a953a77f15dbdf26ff551b0c37fa302203e87299
Josh Hanley <josh@hitsystems.com.au>
2024-06-21 19:30:10 +1000

    Add local namespace test

--- config/livewire-autocomplete.php
+++ config/livewire-autocomplete.php
@@ -4,7 +4,7 @@ return [
     /*
      * Set this to `true` if you want to use the global namespace
      * `<x-autocomplete />` or `false` if you want to use the
-     * package namespace `<x-lwa-autocomplete />`
+     * package namespace `<x-lwa::autocomplete />`
      *
      * This is set to true by default
      */
---
+++ tests/Browser/LocalNamespaceTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace LivewireAutocomplete\Tests\Browser;
+
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+use Livewire\Livewire;
+use LivewireAutocomplete\Tests\TestCase;
+
+class LocalNamespaceTest extends TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('livewire-autocomplete.use_global_namespace', false);
+    }
+
+    public function component()
+    {
+        return new class extends Component
+        {
+            public $input;
+
+            public $selected;
+
+            public function updated()
+            {
+                usleep(500000); // 0.5 seconds
+            }
+
+            #[Computed]
+            public function results()
+            {
+                if ($this->input === null) {
+                    return collect();
+                }
+
+                return collect([
+                    'bob',
+                    'john',
+                    'bill',
+                ])
+                    ->filter(function ($result) {
+                        if (! $this->input) {
+                            return true;
+                        }
+
+                        return str_contains($result, $this->input);
+                    })
+                    ->values()
+                    ->toArray();
+            }
+
+            public function render()
+            {
+                return <<< 'HTML'
+                <div>
+                    <x-lwa::autocomplete wire:model.live="selected" dusk="autocomplete">
+                        <x-lwa::autocomplete-input wire:model.live="input" dusk="input">
+                            <x-lwa::autocomplete-clear-button dusk="clear-button" />
+                        </x-lwa::autocomplete-input>
+
+                        <x-lwa::autocomplete-list dusk="dropdown" x-cloak>
+                            <x-lwa::autocomplete-loading dusk="loading" />
+
+                            @if (!$input && $this->results->isEmpty())
+                                <x-lwa::autocomplete-prompt dusk="prompt" />
+                            @endif
+
+                            @if ($input)
+                                <x-lwa::autocomplete-new-item :value="$input" dusk="add-new" />
+
+                                @forelse($this->results as $key => $result)
+                                    <x-lwa::autocomplete-item :key="$result" :value="$result" dusk="result-{{ $key }}">
+                                        {{ $result }}
+                                    </x-lwa::autocomplete-item>
+                                @empty
+                                    <x-lwa::autocomplete-empty dusk="empty">
+                                        No results found
+                                    </x-lwa::autocomplete-empty>
+                                @endforelse
+                            @endif
+                        </x-lwa::autocomplete-list>
+                    </x-lwa::autocomplete>
+
+                    <div dusk="result-output">{{ $selected }}</div>
+                </div>
+                HTML;
+            }
+        };
+    }
+
+    /** @test */
+    public function all_components_load_successfully_from_local_namespace()
+    {
+        Livewire::visit($this->component())
+            ->waitForLivewireToLoad()
+            ->assertVisible('@autocomplete')
+            ->assertVisible('@input')
+            ->click('@input')
+            ->assertVisible('@dropdown')
+            ->assertVisible('@prompt')
+            ->keys('@input', 'b')
+            ->assertVisible('@loading')
+            ->waitForLivewire()->keys('@input', 'o')
+            ->assertVisible('@add-new')
+            ->assertVisible('@result-0')
+            ->keys('@input', '{BACKSPACE}')
+            ->waitForLivewire()->keys('@input', 'b')
+            ->assertVisible('@empty')
+            ->waitForLivewire()->keys('@input', '{BACKSPACE}')
+            ->keys('@input', '{ARROW_DOWN}')
+            ->keys('@input', '{ARROW_DOWN}')
+            ->waitForLivewire()->keys('@input', '{ENTER}')
+            ->assertVisible('@clear-button');
+    }
+}
